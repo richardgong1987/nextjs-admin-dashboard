ef62288ac6ee166e055ee0ea49d8f301b7d8117b
hjg <richardgong1988@gmail.com>
2025-09-12 21:57:39 +0900

    updated

--- src/app/admin/providers.tsx
+++ src/app/admin/providers.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { SidebarProvider } from "@/components/Layouts/sidebar/sidebar-context";
-import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <SidebarProvider>{children}</SidebarProvider>
-    </ThemeProvider>
-  );
+  return <SidebarProvider>{children}</SidebarProvider>;
 }
